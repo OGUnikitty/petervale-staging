c589b2baf81eaac6145f18592057025e787d968e
OGUnikitty <fitohaluso@gmail.com>
2024-08-22 00:27:51 +0200

    Slider + minor fixes
    * Addition of slider
    
    * Fixing of navbar dropdown causing overflow
    
    *navbar responsiveness overhaul
    
    *PRINT banner changed from .png to font

--- home.js
+++ home.js
@@ -1,7 +1,42 @@
-const menuToggle = document.getElementById('menu-toggle');
-const navLinks = document.querySelector('.nav-links');
+document.addEventListener("DOMContentLoaded", function() {
+  const hamburger = document.querySelector(".hamburger");
+  const navBar = document.querySelector(".navbar");
 
-menuToggle.addEventListener('click', function() {
-  menuToggle.classList.toggle('active');
-  navLinks.classList.toggle('active');
+  hamburger.addEventListener("click", function() {
+      navBar.classList.toggle("active");
+      document.body.classList.toggle("overflow_hidden");
+  });
 });
+
+//SLIDER LOGIC
+
+animateSlider("service__list")
+
+function animateSlider(ulClassName) {
+    const ul = document.querySelector('.' + ulClassName);
+    const li = ul.querySelector('li');
+    const liWidth = li.offsetWidth;
+  
+    const liCount = ul.getElementsByTagName('li').length;
+    console.log(liCount);
+  
+    // Set custom property (optional)
+    ul.style.setProperty('--liCount', liCount);
+    ul.style.setProperty('--liWidth', `-${liWidth}px`);
+
+    //duplucate all <li> children
+    for (let i = 0; i < liCount; i++) {
+      const originalLi = ul.children[i]; // Get the original li in order
+      const newLi = originalLi.cloneNode(true); // Clone with child nodes
+      ul.appendChild(newLi);
+    }
+    //native js animation (possible to be worked on)
+    // ul.animate([
+    //   { transform: 'translateX(0)' },
+    //   { transform: 'translateX(-100%)' }
+    // ], {
+    //   duration: 5000, // 5 seconds
+    //   iterations: Infinity,
+    //   easing: 'linear'
+    // });
+  }
